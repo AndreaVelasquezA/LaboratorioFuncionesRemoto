c71c1d9f1e1d251a6515f7c39b9e7a36b0330b4d
Andrea <andrea.velasquez.2019@upb.edu.co>
2019-09-21 10:14:04 +0200

    primer commit

--- parte3.py
+++ parte3.py
@@ -1,14 +1,19 @@
 print("digite el numero: ")
 def is_prime(n):
   div=0
-  for i in range (1,n+1):
-        if n%i==0:
-         div=div+1
 
-  if div<=2:
-    print("is a prime number")
+for i in range (1,n+1):
+    if n%i==0:
+         div=div+1
+        if n>0:
+          if div<=2:
+            print("1")
+          else:
+            print("2")
   else:
-    print("is not a prime number")
+    print("sorry")
+
 
 n=int(input("digite el numero: "))
+
 is_prime(n)
